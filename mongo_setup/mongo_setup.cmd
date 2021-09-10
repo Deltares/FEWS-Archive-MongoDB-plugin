246cfa99dd9b7e562f3a78f2c91039c6def0b4ec
emroush <emroush@infisys.net>
2021-09-10 18:10:28 +0000

    cleanup win installer.  improved help.  elevated prompt detection, error detection.

--- mongo_setup/mongo_setup.cmd
+++ mongo_setup/mongo_setup.cmd
@@ -2,7 +2,15 @@
 setlocal
 set operation=%1
 
+whoami /groups | find "S-1-16-12288" >log.txt
+set /p is_admin=<log.txt
+if "%is_admin%"=="" (
+	echo This script must be executed from an elevated shell >>log.txt
+	goto error )
+echo. >log.txt
+
 :variables
+
 if not "%2"=="" (
 	set %2=%3
     shift
@@ -15,11 +23,11 @@ goto help
 
 :install
 
-if "%-install_dir%"=="" goto help_install
-if "%-data_dir%"=="" goto help_install
-if "%-log_dir%"=="" goto help_install
-if "%-service_name%"=="" goto help_install
-if "%-port%"=="" goto help_install
+if "%-install_dir%"=="" goto help
+if "%-data_dir%"=="" goto help
+if "%-log_dir%"=="" goto help
+if "%-service_name%"=="" set -service_name="MongoDB"
+if "%-port%"=="" set -port="27017"
 
 echo Creating Directories...
 if not exist %-install_dir% md %-install_dir%
@@ -38,13 +46,20 @@ if %errorlevel%==0 (
 	sc delete %-service_name% >NUL )
 
 echo Acquiring Binaries...
-curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-5.0.2.zip -o .\install\mongodb-windows-x86_64-5.0.2.zip >NUL
+curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-5.0.2.zip -o .\install\mongodb-windows-x86_64-5.0.2.zip 11>>log.txt 2>>&1 2>>&1
+if %errorlevel%==1 goto error
 
 echo Installing MongoDB...
-tar -zxf .\install\mongodb-windows-x86_64-5.0.2.zip -C %-install_dir% >NUL
-xcopy /s /y /q %-install_dir%\mongodb-win32-x86_64-windows-5.0.2\* %-install_dir% >NUL
+tar -zxf .\install\mongodb-windows-x86_64-5.0.2.zip -C %-install_dir% 1>>log.txt 2>>&1
+if %errorlevel%==1 goto error
+
+xcopy /s /y /q %-install_dir%\mongodb-win32-x86_64-windows-5.0.2\* %-install_dir% 1>>log.txt 2>>&1
+if %errorlevel%==1 goto error
+
 rd /s /q %-install_dir%\mongodb-win32-x86_64-windows-5.0.2 >NUL
+
 del %-install_dir%\bin\Install-Compass.ps1 >NUL
+
 ::%-install_dir%\bin\vcredist_x64.exe
 
 echo Writing Configuration...
@@ -64,19 +79,21 @@ echo   authorization: "disabled"
 )>%-install_dir%\bin\mongod.cfg
 
 echo Installing Service...
-%-install_dir%\bin\mongod.exe --install --config %-install_dir%\bin\mongod.cfg --serviceName %-service_name% --serviceDisplayName "MongoDB Server (%-service_name%)" >NUL
+%-install_dir%\bin\mongod.exe --install --config %-install_dir%\bin\mongod.cfg --serviceName %-service_name% --serviceDisplayName "MongoDB Server (%-service_name%)" 1>>log.txt 2>>&1
+if %errorlevel%==1 goto error
 
 echo Starting Service...
-sc start %-service_name% >NUL
+sc start %-service_name% 1>>log.txt 2>>&1
+if %errorlevel%==1 goto error
 
 ping localhost -n 10 >NUL
 
-goto end
+goto summary
 
 :remove
 
-if "%-install_dir%"=="" goto help_remove
-if "%-service_name%"=="" goto help_remove
+if "%-install_dir%"=="" goto help
+if "%-service_name%"=="" goto help
 
 sc query type=service state=all |find "%-service_name%" >NUL && sc query %-service_name% |find "RUNNING" >NUL
 if %errorlevel%==0 (
@@ -92,38 +109,51 @@ if exist %-install_dir% (
 	echo Removing MongoDB...
 	rd /s /q %-install_dir% >NUL )
 
-goto end
+goto summary
 
 :help
 
-echo mongo_setup.cmd install -install_dir install_dir -data_dir data_dir -log_dir log_dir -service_name service_name -port port
-echo mongo_setup.cmd install -install_dir f:\test\install -data_dir f:\test\data -log_dir f:\test\log -service_name TestFEWSArchiveMongoDB -port 27019
-
-echo mongo_setup.cmd remove -install_dir install_dir -service_name service_name
-echo mongo_setup.cmd remove -install_dir f:\test\install -service_name TestFEWSArchiveMongoDB
+echo.
+echo HELP:
+echo mongo_setup.cmd [command] -[options]
+echo.
+echo ========================================================
+echo [command]: install
+echo ========================================================
+echo [options]:
+echo   -install_dir: where to place the Mongo DB binaries.
+echo   -data_dir: where to place the Mongo DB data files.
+echo   -log_dir: where to place the Mongo DB log files.
+echo   -service_name: the name of the service running MongoDB. Default=MongoDB.
+echo   -port: the port to use for accessing Mongo DB. Default=27217.
+echo.
+echo EXAMPLE:
+echo mongo_setup.cmd install -install_dir c:\test\install -data_dir c:\test\data -log_dir c:\test\log -service_name TestFEWSArchiveMongoDB -port 27019
+echo.
+echo.
+echo ======================================================== 
+echo [commnad]: remove 
+echo ========================================================
+echo [options]:
+echo   -install_dir: where the Mongo DB binaries were installed.
+echo   -service_name: the name of the service running MongoDB.
+echo.
+echo EXAMPLE:
+echo mongo_setup.cmd remove -install_dir c:\test\install -service_name TestFEWSArchiveMongoDB
+echo.
+echo.
 
 goto end
 
-:help_install
+:summary
 
-echo mongo_setup.cmd install -install_dir install_dir -data_dir data_dir -log_dir log_dir -service_name service_name -port port
-echo -install_dir="%-install_dir%"
-echo -data_dir="%-data_dir%"
-echo -log_dir="%-log_dir%"
-echo -service_name="%-service_name%"
-echo -port="%-port%"
-
+echo Complete!
 goto end
 
-:help_remove
+:error
 
-echo mongo_setup.cmd remove -install_dir install_dir -service_name service_name
-echo -install_dir="%-install_dir%"
-echo -service_name="%-service_name%"
-
+echo [%*] 
+type log.txt
 goto end
 
 :end
-
-echo [%*] 
-echo Complete!
