88bc5857c272861b57587f9a1ff24613a7365d6b
emroush <emroush@infisys.net>
2024-02-29 20:20:11 +0000

    reorg repo

--- archive-database/archive-database/src/test/java/nl/fews/archivedatabase/mongodb/query/operations/FilterTest.java
+++ archive-database/archive-database/src/test/java/nl/fews/archivedatabase/mongodb/query/operations/FilterTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Testcontainers
 class FilterTest {
 	@Container
-	public MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:5.0.12"));
+	public MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:6.0"));
 
 	@BeforeEach
 	public void setUpClass(){
