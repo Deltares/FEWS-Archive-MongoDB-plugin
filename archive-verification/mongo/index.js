c28db647b3f931de9dd2573a7b00d0880c88680a
Ed Roush <emroush@gmail.com>
2024-04-03 17:38:09 -0400

    performance refinements

--- archive-verification/mongo/index.js
+++ archive-verification/mongo/index.js
@@ -4,7 +4,13 @@ db.SimulatedHistoricalScalarTimeSeriesStitched.createIndexes([
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "startTime": 1, "endTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "startTime": 1, "endTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1},
-    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1}
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "localStartTime": 1, "localEndTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "localStartTime": 1, "localEndTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "startTime": 1, "endTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "startTime": 1, "endTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "ensembleId": 1}
 ]);
 
 db.ExternalHistoricalScalarTimeSeries.createIndexes([
@@ -22,7 +28,13 @@ db.SimulatedHistoricalScalarTimeSeries.createIndexes([
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1},
-    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1}
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "ensembleId": 1}
 ]);
 
 db.SimulatedForecastingScalarTimeSeries.createIndexes([
@@ -31,7 +43,13 @@ db.SimulatedForecastingScalarTimeSeries.createIndexes([
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1},
-    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1}
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "ensembleId": 1}
 ]);
 
 db.ExternalForecastingScalarTimeSeries.createIndexes([
@@ -40,6 +58,12 @@ db.ExternalForecastingScalarTimeSeries.createIndexes([
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1},
     {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1},
-    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1}
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "localForecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "forecastTime": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "locationId": 1, "ensembleId": 1},
+    {"moduleInstanceId": 1,"parameterId": 1,"qualifierId": 1,"encodedTimeStepId": 1, "ensembleId": 1}
 ]);
 
