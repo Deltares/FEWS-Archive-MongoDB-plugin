2fde13d3b3b002ec0a1af360ac6f332bea689821
emroush <emroush@infisys.net>
2025-05-27 18:34:39 +0000

    fixed timeseries header none check

--- archive-database/archive-database/src/main/java/nl/fews/archivedatabase/mongodb/query/MongoDbArchiveDatabaseTimeSeriesReader.java
+++ archive-database/archive-database/src/main/java/nl/fews/archivedatabase/mongodb/query/MongoDbArchiveDatabaseTimeSeriesReader.java
@@ -81,7 +81,7 @@ public class MongoDbArchiveDatabaseTimeSeriesReader implements ArchiveDatabaseTi
 		List<TimeSeriesArray<TimeSeriesHeader>> timeSeriesArraysResults = Collections.synchronizedList(new ArrayList<>());
 		IntStream.range(0, timeSeriesArrays.size()).parallel().forEach(i -> {
 			TimeSeriesArray<TimeSeriesHeader> timeSeriesArray = timeSeriesArrays.get(i);
-			if(!timeSeriesArray.getHeader().equals(TimeSeriesHeader.NONE)) {
+			if(!timeSeriesArray.getHeader().isNone()) {
 				Box<TimeSeriesArray<TimeSeriesHeader>, SystemActivityDescriptor> timeSeriesArrayResult = importStitchedSimulatedHistorical(period, timeSeriesArray.getHeader());
 				if (timeSeriesArrayResult != null && !TimeSeriesArrayUtil.timeSeriesArrayValuesAreEqual(timeSeriesArray, timeSeriesArrayResult.getObject0()))
 					timeSeriesArraysResults.add(timeSeriesArrayResult.getObject0());
@@ -144,7 +144,7 @@ public class MongoDbArchiveDatabaseTimeSeriesReader implements ArchiveDatabaseTi
 		List<TimeSeriesArray<TimeSeriesHeader>> timeSeriesArraysResults = Collections.synchronizedList(new ArrayList<>());
 		IntStream.range(0, timeSeriesArrays.size()).parallel().forEach(i -> {
 			TimeSeriesArray<TimeSeriesHeader> timeSeriesArray = timeSeriesArrays.get(i);
-			if(!timeSeriesArray.getHeader().equals(TimeSeriesHeader.NONE)) {
+			if(!timeSeriesArray.getHeader().isNone()) {
 				Box<TimeSeriesArray<TimeSeriesHeader>, SystemActivityDescriptor> timeSeriesArrayResult = importExternalHistorical(period, timeSeriesArray.getHeader());
 				if (timeSeriesArrayResult != null && !TimeSeriesArrayUtil.timeSeriesArrayValuesAreEqual(timeSeriesArray, timeSeriesArrayResult.getObject0()))
 					timeSeriesArraysResults.add(timeSeriesArrayResult.getObject0());
