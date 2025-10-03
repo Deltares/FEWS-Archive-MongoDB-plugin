7c92a76596953d75b1e31fccf4cf4c9ee71de279
emroush <emroush@infisys.net>
2025-10-03 15:12:36 +0000

    updating installer / run paradigm

--- archive-graphcast/package.cmd
+++ archive-graphcast/package.cmd
@@ -1,5 +1,10 @@
 :: Copyright (c) 2025 INFISYS INC
 
 @echo off
-git clean -fdx
+:: git clean -fdx
+if exist ".\decorator\__pycache__" rmdir .\decorator\__pycache__ /S /Q
+if exist ".\model\__pycache__" rmdir .\model\__pycache__ /S /Q
+if exist ".\sources\__pycache__" rmdir .\sources\__pycache__ /S /Q
+if exist ".\graphcast.zip" del .\graphcast.zip /Q
+
 install\7za a graphcast.zip decorator model sources graphcast.cmd run_graphcast.py
--- archive-graphcast/package.sh
+++ archive-graphcast/package.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Copyright (c) 2025 INFISYS INC
+# git clean -fdx
+if [ -d "./common/__pycache__" ]; then rm -rf ./common/__pycache__;fi
+if [ -d "./decorator/__pycache__" ]; then rm -rf ./decorator/__pycache__;fi
+if [ -d "./model/__pycache__" ]; then rm -rf ./model/__pycache__; fi
+if [ -d "./sources/__pycache__" ]; then rm -rf ./sources/__pycache__; fi
 
-git clean -fdx
 zip -rq graphcast.zip common decorator model sources graphcast.sh run_graphcast.py
