5afd45b59f1ccbfc6fa3b3b777665ebfdf7bfbd3
emroush <emroush@infisys.net>
2025-10-10 04:09:14 +0000

    adjusted deployment to be self-contained

--- archive-graphcast/build_manylinux.sh
+++ archive-graphcast/build_manylinux.sh
@@ -11,11 +11,10 @@ tar -xzf install/cpython*.tar.gz -C build
 
 docker run --rm -v "$PWD:/io" quay.io/pypa/manylinux2014_x86_64 bash -lc "
     set -e
-    cd /opt/python/cp313-cp313/bin
-    python -m pip install --upgrade pip wheel build auditwheel
-    pip wheel -r /io/requirements.txt -w /io/wheelhouse
-    pip wheel /io/models/GraphCastOperationalIfs/graphcast.zip -w /io/wheelhouse
-    pip wheel /io/. -w /io/wheelhouse --no-build-isolation
+    /opt/python/cp313-cp313/bin/python -m pip install --upgrade pip wheel build auditwheel
+    /opt/python/cp313-cp313/bin/pip wheel -r /io/requirements.txt -w /io/wheelhouse
+    /opt/python/cp313-cp313/bin/pip wheel /io/models/GraphCastOperationalIfs/graphcast.zip -w /io/wheelhouse
+    /opt/python/cp313-cp313/bin/pip wheel /io/. -w /io/wheelhouse --no-build-isolation
     auditwheel repair /io/wheelhouse/*.whl -w /io/wheelhouse || true
   "
 
@@ -32,7 +31,7 @@ build/python/bin/python -m pip freeze > frozen_requirements.txt
 \cp -f graphcast.sh build/
 
 # CLEANUP
-rm -rf tva_graphcast.egg-info build/bdist.linux-x86_64 build/lib
+rm -rf wheelhouse tva_graphcast.egg-info build/bdist.linux-x86_64 build/lib
 
 # BUILD ENVIRONMENT
 # tar -czf graphcast.tar.gz -C build .
