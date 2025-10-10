74048aa59ceb878af1dd59e9eb2130280f9669ca
emroush <emroush@infisys.net>
2025-10-10 06:41:14 +0000

    adjusted deployment to be self-contained

--- archive-graphcast/build_manylinux.sh
+++ archive-graphcast/build_manylinux.sh
@@ -9,23 +9,29 @@ mkdir build wheelhouse
 # BUILD PYTHON ENVIRONMENT
 tar -xzf install/old/cpython-3.12*.tar.gz -C build
 
-docker run --rm -v "$PWD:/io" quay.io/pypa/manylinux2014_x86_64 bash -lc '
-  set -e
-  /opt/python/cp312-cp312/bin/python -m pip install --upgrade pip wheel build setuptools auditwheel
-  /opt/python/cp312-cp312/bin/pip wheel -r /io/requirements.txt -w /io/wheelhouse
-  /opt/python/cp312-cp312/bin/pip wheel /io/models/GraphCastOperationalIfs/graphcast.zip -w /io/wheelhouse
-  /opt/python/cp312-cp312/bin/pip wheel /io/. -w /io/wheelhouse --no-deps --no-build-isolation
-  auditwheel repair /io/wheelhouse/*.whl -w /io/wheelhouse || true
+#docker run --rm -v "$PWD:/io" quay.io/pypa/manylinux2014_x86_64 bash -lc '
+#  set -e
+#  /opt/python/cp312-cp312/bin/python -m pip install --upgrade pip wheel build setuptools auditwheel
+#  /opt/python/cp312-cp312/bin/pip wheel -r /io/requirements.txt -w /io/wheelhouse
+#  /opt/python/cp312-cp312/bin/pip wheel /io/models/GraphCastOperationalIfs/graphcast.zip -w /io/wheelhouse
+#  /opt/python/cp312-cp312/bin/pip wheel /io/. -w /io/wheelhouse --no-deps --no-build-isolation
+#  auditwheel repair /io/wheelhouse/*.whl -w /io/wheelhouse || true
+#
+#  #cp -a /usr/lib64/libstdc++.so.6.* /io/build/python/lib/
+#  #cd /io/build/python/lib
+#  #ln -sfn "$(ls -1 libstdc++.so.6.* | sort | tail -n1)" libstdc++.so.6
+#'
 
-  #cp -a /usr/lib64/libstdc++.so.6.* /io/build/python/lib/
-  #cd /io/build/python/lib
-  #ln -sfn "$(ls -1 libstdc++.so.6.* | sort | tail -n1)" libstdc++.so.6
-'
+#build/python/bin/python -m pip install --root-user-action ignore --upgrade pip
+#build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse -r requirements.txt
+#build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse --upgrade models/GraphCastOperationalIfs/graphcast.zip
+#build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse --no-build-isolation .
+#build/python/bin/python -m pip freeze > frozen_requirements.txt
 
 build/python/bin/python -m pip install --root-user-action ignore --upgrade pip
-build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse -r requirements.txt
-build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse --upgrade models/GraphCastOperationalIfs/graphcast.zip
-build/python/bin/python -m pip install --root-user-action ignore --no-index --find-links wheelhouse --no-build-isolation .
+build/python/bin/python -m pip install --root-user-action ignore -r requirements.txt
+build/python/bin/python -m pip install --root-user-action ignore --upgrade models/GraphCastOperationalIfs/graphcast.zip
+build/python/bin/python -m pip install --root-user-action ignore --no-build-isolation .
 build/python/bin/python -m pip freeze > frozen_requirements.txt
 
 # ADD MODELS
