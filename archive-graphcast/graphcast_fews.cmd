474bc6775838d2281edb806ecdb0006af0186eee
emroush <emroush@infisys.net>
2025-12-22 18:46:47 +0000

    tyy build local to force old openssl wheel refs out.  do not work with fips mode on rhel9.7

--- archive-graphcast/graphcast_fews.cmd
+++ archive-graphcast/graphcast_fews.cmd
@@ -2,7 +2,7 @@
 :: Copyright (c) 2025 INFISYS INC
 
 :: [config_path] [model_name]
-:: graphcast.cmd "c:\_GIT\FEWS-Archive\archive-graphcast\configurations\gfs_win_config.xml" "GraphCastOperationalGfs"
+:: graphcast_fews.cmd "c:\_GIT\FEWS-Archive\archive-graphcast\configurations\gfs_win_config.xml" "GraphCastOperationalGfs"
 
 @echo off
 
--- archive-graphcast/graphcast_fews.sh
+++ archive-graphcast/graphcast_fews.sh
@@ -2,10 +2,10 @@
 # Copyright (c) 2025 INFISYS INC
 
 # [config_path] [model_name]
-# ./graphcast.sh "/root/_git/FEWS-Archive/archive-graphcast/configurations/gfs_posix_config.xml" "GraphCastOperationalGfs"
+# ./graphcast_fews.sh "/root/_git/FEWS-Archive/archive-graphcast/configurations/gfs_posix_config.xml" "GraphCastOperationalGfs"
 
 set -e
 
 cd ~/graphcast
 
-source ./graphcast.sh "$1" "$2"
+bash ./graphcast.sh "$1" "$2"
